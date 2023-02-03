36fdc689c8493d2fe78efed79d9e56e838e14af3
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-02-04 08:47:57 +0900

    initial commit

---
+++ lib/visa_ex.ex
@@ -0,0 +1,18 @@
+defmodule VisaEx do
+  @moduledoc """
+  Documentation for `VisaEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VisaEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/visa_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule VisaExTest do
+  use ExUnit.Case
+  doctest VisaEx
+
+  test "greets the world" do
+    assert VisaEx.hello() == :world
+  end
+end
