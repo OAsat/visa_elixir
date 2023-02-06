ab1caeb596b495ea026519149a848f793de364e8
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-02-06 13:35:42 +0900

    list resources

--- lib/visa_ex.ex
+++ lib/visa_ex.ex
@@ -1,8 +1,5 @@
 defmodule VisaEx do
   use Rustler, otp_app: :visa_ex, crate: "visa_ex_nif"
 
-  # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
-
-  def list_instr(), do: :erlang.nif_error(:nif_not_loaded)
+  def list_resources(), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/visa_ex_nif/src/lib.rs
+++ native/visa_ex_nif/src/lib.rs
@@ -1,43 +1,22 @@
+use rustler::{Error, NifResult};
 use std::ffi::CString;
-use visa_rs::{DefaultRM, AsResourceManager, Result as VResult, Error as VError};
-use rustler::{Error as RError};
+use visa_rs::DefaultRM;
 
-mod atoms {
-    rustler::atoms! {
-        ok,
-        error,
-        unknown
-    }
+fn convert_visa_error(error: visa_rs::Error) -> Error {
+    Error::Term(Box::new(error.0.to_string()))
 }
 
 #[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
-}
-
-fn to_rustler_error(visa_rs_error: VError) -> RError {
-    RError::Term(Box::new(visa_rs_error.0.to_string()))
-}
-
-fn _list_instr() -> VResult<Vec<String>> {
-    let rm = DefaultRM::new()?;
-    let mut list = rm.find_res_list(&CString::new("?*INSTR").unwrap().into())?;
+fn list_resources() -> NifResult<Vec<String>> {
+    let rm = DefaultRM::new().map_err(convert_visa_error)?;
+    let mut list = rm
+        .find_res_list(&CString::new("?*INSTR").unwrap().into())
+        .map_err(convert_visa_error)?;
     let mut vias_vec = Vec::new();
-    while let Some(n) = list.find_next()? {
+    while let Some(n) = list.find_next().map_err(convert_visa_error)? {
         vias_vec.push(n.to_string());
-    };
+    }
     Ok(vias_vec)
 }
 
-#[rustler::nif]
-fn list_instr() -> Result<Vec<String>, RError> {
-    let list = match _list_instr() {
-        Ok(success) => {
-            success
-        },
-        Err(err) => return Err(to_rustler_error(err))
-    };
-    Ok(list)
-}
-
-rustler::init!("Elixir.VisaEx", [add, list_instr]);
+rustler::init!("Elixir.VisaEx", [list_resources]);
