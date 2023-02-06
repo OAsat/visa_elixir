7157f7b842659d018d00a83dfeeb27eaa8f119d7
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-02-06 16:25:14 +0900

    add query function

--- lib/visa_ex.ex
+++ lib/visa_ex.ex
@@ -2,10 +2,20 @@ defmodule VisaEx do
   defmodule Native do
     use Rustler, otp_app: :visa_ex, crate: "visa_ex_nif"
 
-    def list_resources(_query), do: :erlang.nif_error(:nif_not_loaded)
+    def list_resources(_message), do: :erlang.nif_error(:nif_not_loaded)
+
+    def query(_address, _message), do: :erlang.nif_error(:nif_not_loaded)
   end
 
-  def list_resources(query \\ "?*::INSTR") do
-    Native.list_resources(query)
+  def list_resources(message \\ "?*::INSTR") do
+    Native.list_resources(message)
+  end
+
+  def query(address, message) do
+    Native.query(address, message)
+  end
+
+  def idn(address) do
+    query(address, "*IDN?\n")
   end
 end
