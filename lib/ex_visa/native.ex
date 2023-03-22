401ef7c52048049fbc2dcf0f0aedfe5b936a11ac
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-03-22 17:48:13 +0900

    minor corrections

--- lib/ex_visa/native.ex
+++ lib/ex_visa/native.ex
@@ -1,7 +1,6 @@
 defmodule ExVisa.Native do
-  @moduledoc """
-  Rustler NIFs.
-  """
+  @moduledoc false
+
   use Rustler, otp_app: :ex_visa, crate: "visa_nif"
 
   def list_resources(_message), do: :erlang.nif_error(:nif_not_loaded)
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule ExVISA.MixProject do
+defmodule ExVisa.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @repo "https://github.com/OAsat/visa_elixir"
 
   def project do
