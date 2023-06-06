c65b72d78c3dd8a6ee372a86680c46c10e981791
SHCommon <shibauchilab@gmail.com>
2023-06-06 18:38:45 +0900

    list resources runs

--- lib/ex_visa/python_visa.ex
+++ lib/ex_visa/python_visa.ex
@@ -3,19 +3,33 @@ defmodule ExVisa.PythonVisa do
 
   python_src = "./python"
 
-  {python_exec, 0} =
+  python_exec =
     case :os.type() do
       {:win32, _} ->
+        System.cmd("cmd.exe", ["/c", "poetry", "env", "use", "3.10"], cd: python_src)
         System.cmd("cmd.exe", ["/c", "poetry", "install"], cd: python_src)
-        System.cmd("cmd.exe", ["/c", "poetry", "run", "where.exe", "python"], cd: python_src)
+
+        {exec_path, 0} =
+          System.cmd("cmd.exe", ["/c", "poetry", "run", "where.exe", "python"], cd: python_src)
+
+        exec_path |> String.split("\r\n") |> List.first() |> String.replace("\\", "/")
 
       {:unix, _} ->
         System.cmd("poetry", ["install"], cd: python_src, into: IO.stream())
-        System.cmd("poetry", ["run", "which", "python"], cd: python_src)
+        {exec_path, 0} = System.cmd("poetry", ["run", "which", "python"], cd: python_src)
+        String.trim(exec_path)
     end
 
   @python_path to_charlist(python_src)
   @python_exec to_charlist(String.trim(python_exec))
+
+  def python_path() do
+    @python_path
+  end
+
+  def python_exec() do
+    @python_exec
+  end
 
   @impl ExVisa.VisaBehaviour
   def init_state(_port_name) do
