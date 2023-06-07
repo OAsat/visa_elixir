e0e700c6d0c04daab1d26ceac45e1bd44cb1c930
SHCommon <shibauchilab@gmail.com>
2023-06-07 12:24:28 +0900

    query sccess

--- python/pyvisa_ex/communicate.py
+++ python/pyvisa_ex/communicate.py
@@ -6,8 +6,12 @@ def list_resources(query):
 
 
 def query(address, message):
-    return pyvisa.ResourceManager().open_resource(address).query(message)
+    address_str = address.decode("utf-8")
+    message_str = message.decode("utf-8")
+    return pyvisa.ResourceManager().open_resource(address_str).query(message_str)
 
 
 def write(address, message):
-    return pyvisa.ResourceManager().open_resource(address).write(message)
+    address_str = address.decode("utf-8")
+    message_str = message.decode("utf-8")
+    return pyvisa.ResourceManager().open_resource(address_str).write(message_str)
