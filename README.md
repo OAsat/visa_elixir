739dd32c38c7968b9fa8e758304780f192405950
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-07-12 11:42:05 +0900

    mod package files

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Add `ex_visa` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:ex_visa, "~> 0.2.0"}
+    {:ex_visa, "~> 0.2.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExVisa.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @repo "https://github.com/OAsat/visa_elixir"
 
   def project do
@@ -50,7 +50,8 @@ defmodule ExVisa.MixProject do
         "native/visa_nif/.cargo",
         "native/visa_nif/src",
         "native/visa_nif/Cargo*",
-        "python/pyvisa_ex/**/*.py",
+        "python/pyvisa_ex/communicate.py",
+        "python/pyvisa_ex/__init__.py",
         "python/poetry.toml",
         "python/poetry.lock",
         "python/pyproject.toml",
